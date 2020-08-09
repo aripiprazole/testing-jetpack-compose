95814e6131bb32186229dbb9e0c17f22677ec96a
Lorenzo Guimaraes de Oliveira <lorenzooguimaraes@gmail.com>
2020-08-09 20:51:02 -0300

    refactor: remove RetrofitService that is not the same generic type of service kind

--- app/src/main/java/com/lorenzoog/testingjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/lorenzoog/testingjetpackcompose/MainActivity.kt
@@ -6,10 +6,18 @@ import androidx.ui.core.setContent
 import androidx.ui.material.MaterialTheme
 import androidx.ui.material.Surface
 import com.lorenzoog.testingjetpackcompose.services.GithubService
-import com.lorenzoog.testingjetpackcompose.services.createRetrofit
 import com.lorenzoog.testingjetpackcompose.ui.TestingJetpackComposeTheme
 import com.lorenzoog.testingjetpackcompose.ui.app.App
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.create
+
+fun createRetrofit(): Retrofit {
+  return Retrofit.Builder()
+    .baseUrl("https://api.github.com")
+    .addConverterFactory(GsonConverterFactory.create())
+    .build()
+}
 
 val retrofit = createRetrofit()
 val githubService = retrofit.create<GithubService>()
